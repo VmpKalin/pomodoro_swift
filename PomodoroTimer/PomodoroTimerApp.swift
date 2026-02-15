c9aaec4c2451c2f07b5611e6cc2e1cbd0a585e65
a.holoiad <artik171198@gmail.com>
2026-02-15 01:08:24 +0100

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Artur Holoiad on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
